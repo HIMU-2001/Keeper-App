a51e3604d3d10d22048790aab2ff3039adb22cc1
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2022-08-15 10:29:23 +0530

    Updates

---
+++ precache-manifest.8b6eca2aefeeb55bf830d0c735c7a2d2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5346735efd27a595fde94c102d8ef72c",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "/Keeper-App/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "e34076a99f9d91430390",
+    "url": "/Keeper-App/static/js/main.571b53c0.chunk.js"
+  },
+  {
+    "revision": "53168b6972fe4da5e554",
+    "url": "/Keeper-App/static/js/runtime-main.5f8222f9.js"
+  }
+]);
